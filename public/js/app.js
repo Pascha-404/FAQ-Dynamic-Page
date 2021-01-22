9fd2c62d32bdbffbbc8ef89234215d7602e81041
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-01-22 15:06:48 +0100

    coded logic for animation of form

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,14 @@
 const buttons = document.querySelectorAll('.faq-toggle');
+const addBtn = document.querySelector('#add');
+const faqFormAdd = document.querySelector('.faq-form');
 
 buttons.forEach(button => {
     button.addEventListener('click', () => {
         button.parentNode.classList.toggle('active')
     })
+})
+
+addBtn.addEventListener('click', () => {
+    faqFormAdd.classList.toggle('active');
+    addBtn.classList.toggle('active');
 })
