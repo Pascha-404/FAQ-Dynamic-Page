b4637da58de805855b8d86bb7f25987b7a44b2c8
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-01-22 15:29:48 +0100

    added post behavior for form

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const express = require('express');
 const app = express();
 const port = 8080;
 const path = require('path');
-const faqData = require('./data.json')
+const faqData = require('./data.json');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
+app.use(express.json())
+app.use(express.urlencoded({
+    extended: true
+}))
 
 app.use(express.static(path.join(__dirname, 'public')))
 
@@ -17,4 +21,16 @@ app.get('/', (req, res) => {
     res.render('index.ejs', {
         faqData
     })
+})
+
+app.post('/', (req, res) => {
+    const {
+        title,
+        text
+    } = req.body;
+    faqData.unshift({
+        title,
+        text
+    });
+    res.redirect('/')
 })
